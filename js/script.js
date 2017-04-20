b2ac58313b90b854345f21d28f79a61c3f226a6d
S. Manongga <smanongga@gmail.com>
2017-04-20 21:41:08 +1200

    Delete getButtonValue function. Declare new function called evalButton

--- js/script.js
+++ js/script.js
@@ -7,52 +7,10 @@ var operationValue;
 window.onload = myCalculator;
 
 function myCalculator() {
-  document.getElementById("calculator-button").onclick = getButtonValue;
+  document.getElementById("calculator-button").onclick = evalButton;
 }
-function getButtonValue(e) {
-  if (e.target.className === 'number') {
-    if(tempString.length > 0 && isNaN(tempString[tempString.length - 1]) === false) {
-      var lastIndex = tempString[tempString.length-1] + e.target.id;
-       tempString[tempString.length - 1] = lastIndex;
-    }
-    else {
-    tempString.push(e.target.id)
-    }
-    document.getElementById("calculator-display").innerHTML = tempString.join("");
-  }
-  if (e.target.className === 'operation' && e.target.id != "=" && currentValue === undefined) {
-    operationValue = e.target.id ;
-    if(tempString[tempString.length - 1] === "-" || tempString[tempString.length - 1] === "/" || tempString[tempString.length - 1] === "*" || tempString[tempString.length - 1] === "+") {
-    tempString.splice(-1,1);
-    tempString.push(operationValue);
-    }
-    else {
-       tempString.push(operationValue);
-    }
-    document.getElementById("calculator-display").innerHTML = tempString.join("");
-  }
-  if (e.target.className === 'operation' && e.target.id != "=" && currentValue != undefined) {
-    tempString.push(currentValue);
-    operationValue = e.target.id ;
-    if(tempString[tempString.length - 1] === "-" || tempString[tempString.length - 1] === "/" || tempString[tempString.length - 1] === "*" || tempString[tempString.length - 1] === "+") {
-    tempString.splice(-1,1);
-    tempString.push(operationValue);
-    }
-    else {
-       tempString.push(operationValue);
-    }
-    console.log(tempString);
-    document.getElementById("calculator-display").innerHTML = tempString.join("");
-  }
-  if(e.target.id === "=") {
-    currentValue = eval(tempString.join(""));
-    document.getElementById("calculator-display").innerHTML = currentValue;
-    tempString =[];
-    console.log(currentValue);
-  }
-  if(e.target.id === "clear") {
-    tempString =[];
-    currentValue = 0;
-    document.getElementById("calculator-display").innerHTML = currentValue;
+function evalButton(e) {
+  if (e.target.className === 'operation') {
+    processOperation(e.target.id);
   }
 }
