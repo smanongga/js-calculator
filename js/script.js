a97a3c1de13d0a4f58987f4661fcb945d8e18cbc
S. Manongga <smanongga@gmail.com>
2017-04-20 21:04:15 +1200

    Add if statement to check number when number button is clicked - push value to display

--- js/script.js
+++ js/script.js
@@ -10,5 +10,14 @@ function myCalculator() {
   document.getElementById("calculator-button").onclick = getButtonValue;
 }
 function getButtonValue(e) {
-
+  if (e.target.className === 'number') {
+    if(tempString.length > 0 && isNaN(tempString[tempString.length - 1]) === false) {
+      var lastIndex = tempString[tempString.length-1] + e.target.id;
+       tempString[tempString.length - 1] = lastIndex;
+    }
+    else {
+    tempString.push(e.target.id)
+    }
+    document.getElementById("calculator-display").innerHTML = tempString.join("");
+  }
 }
